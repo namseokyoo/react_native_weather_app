df36ddb3a6d4c30f60a2992dd742c93588989557
namseokyoo <namseok.yoo@gmail.com>
2021-02-01 21:36:39 +0900

    get location from expo-location

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Alert } from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// export default function App() {
+//     return <Loading />;
+// }
+
+export default class extends React.Component {
+    state = {
+        isLoading: true,
+    };
+    getLocation = async () => {
+        try {
+            // throw Error();
+            await Location.requestPermissionsAsync();
+            const {
+                coords: { latitude, longitude },
+            } = await Location.getCurrentPositionAsync();
+            this.setState({ isLoading: false });
+            //Send to API and get Weather
+            // console.log(coords.latitude, coords.longitude);
+        } catch (error) {
+            Alert.alert("Can't find your position.", "So sad");
+        }
+    };
+    componentDidMount() {
+        this.getLocation();
+    }
+    render() {
+        const { isLoading } = this.state;
+        return isLoading ? <Loading /> : null;
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
